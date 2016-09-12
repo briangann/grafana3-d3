4c6fa98453201c0e8b4c58fa67b16b84446ebb82
Brian Gann <brian.gann@ge.com>
2016-09-12 10:13:55 -0500

    now handles resizing panel, but not perfect...

--- src/d3-graph-panel/ctrl.js
+++ src/d3-graph-panel/ctrl.js
@@ -3,7 +3,6 @@ import _ from 'lodash';
 import $ from 'jquery';
 import * as d3 from './external/d3.v4.min.js';
 import './css/d3_graph.css!';
-import d3graphRenderer from './renderer';
 
 
 export class D3GraphPanelCtrl extends MetricsPanelCtrl {
@@ -17,21 +16,8 @@ export class D3GraphPanelCtrl extends MetricsPanelCtrl {
 
     console.log("D3GraphPanelCtrl constructor!");
     this.events.on('init-edit-mode', this.onInitEditMode.bind(this));
+    this.events.on('render', this.onRender.bind(this));
     console.log("D3GraphPanelCtrl constructor done!");
-    var tmpPanelHeight = $scope.ctrl.panel.height;
-    if (typeof tmpPanelHeight === 'undefined') {
-      // get from the row instead
-      tmpPanelHeight = $scope.ctrl.row.height;
-      if (typeof tmpPanelHeight === 'undefined') {
-        tmpPanelHeight = "250px";
-      }
-    }
-    // lastly remove the px from our height, we don't need it...
-    tmpPanelHeight = tmpPanelHeight.replace("px","");
-    this.panelHeight = parseInt(tmpPanelHeight);
-    var tmpPanelWidthPx = $("div.panel").css("width");
-    tmpPanelWidthPx = tmpPanelWidthPx.replace("px","");
-    this.panelWidth = parseInt(tmpPanelWidthPx);
   }
 
   // setup the editor
@@ -49,132 +35,74 @@ export class D3GraphPanelCtrl extends MetricsPanelCtrl {
     this.panelContainer = container;
   }
 
-  onRender() {
-    console.log("d3graph onRender entered");
-    //debugger;
-    if (this.panelContainer === null) {
-      console.log("d3graph panel container object null");
-      return;
-    }
-    if (this.initialized) {
-      var tmpPanelWidthPx = $("div.panel").css("width");
-      tmpPanelWidthPx = tmpPanelWidthPx.replace("px","");
-      this.panelWidth = parseInt(tmpPanelWidthPx);
-      //debugger;
-      //var pc = $("div.panel-container");
-      //var tmpPanelHeightPx = $("div.panel-container").css("height");
-      //var svgElement = $("#mystuff");
-      //var svgElementHeight = $("#mystuff").css("height", "550px");
-      //var svgElementWidth = $("#mystuff").css("width", "1200px");
-      //svgElement.height = tmpPanelHeightPx;
-      //tmpPanelHeightPx = tmpPanelHeightPx.replace("px","");
-      //this.panelHeight = parseInt(tmpPanelHeightPx);
-      console.log("d3graph panel is already initialized");
-      //var myCenter = d3.event.transform(-417.611,64.791,2.209);
-      //this.svg.attr("transform", "translate(-417.611,64.791,2.209)");
-      return;
-    }
-    this.initializeD3();
-    // apply our zoom
-    //this.svg.attr("transform", "translate(-417.611,64.791)scale(2.209)");
-    this.initialized = true;
+  getPanelWidth() {
+    // locate this panel with jQuery
+    var tmpPanelWidthCSS = $("div.panel").css("width");
+    var tmpPanelWidthPx = tmpPanelWidthCSS.replace("px","");
+    var tmpPanelWidth = parseInt(tmpPanelWidthPx);
+    // get our "span" setting
+    var percentWidth = ((this.panel.span / 1.2) * 10) / 100;
+    // calculate actual width
+    var actualWidth = tmpPanelWidth * percentWidth;
+    return actualWidth;
   }
 
-  initializeD3() {
-    console.log("initializeD3");
-    //var width = 1400;
-    //var height = 500;
+  getPanelHeight() {
+    // panel can have a fixed height via options
+    var tmpPanelHeight = this.$scope.ctrl.panel.height;
+    // if that is blank, try to get it from our row
+    if (typeof tmpPanelHeight === 'undefined') {
+      // get from the row instead
+      tmpPanelHeight = this.row.height;
+      // default to 250px if that was undefined also
+      if (typeof tmpPanelHeight === 'undefined') {
+        tmpPanelHeight = "250px";
+      }
+    }
+    // convert to numeric value
+    tmpPanelHeight = tmpPanelHeight.replace("px","");
+    var actualHeight = parseInt(tmpPanelHeight);
+    // grafana minimum height for a panel is 250px
+    if (actualHeight < 250) {
+      actualHeight = 250;
+    }
+    return actualHeight;
+  }
 
+  onRender() {
+    console.log("Render D3");
     // use jQuery to get the height on our container
-    //$('#someDiv').height();
-    //debugger;
-    //var ugh = this.panelContainer[0].containerHeight;
-    //debugger;
-    //var meh = $('#panel-container');
-    //var meh_h = meh.outerHeight();
-    //var meh2 = $('#panel-container').css("minHeight");
-    //var meh4 = $('#panel-container').css("containerHeight");
-    //var meh3 = $('#panel-container')[0].css("min-height");
-    //debugger;
-    //var meh1 = $("div.panel");
-    // locate our panel?
-    var tmpPanelWidthPx = $("div.panel").css("width");
-    //var tmpPanelWidthPx = meh1.css("width");
-    //var tmpWPx = this.panelContainer;
-    //var ugh = tmpWPx.css("width");
-    tmpPanelWidthPx = tmpPanelWidthPx.replace("px","");
-    this.panelWidth = parseInt(tmpPanelWidthPx);
-
-    //var meh2 = $('#panel').outerHeight();
-    //var meh3 = $('#panel').outerHeight();
-
-    //var meh3 = $("#panel-container").style;
-
-    //var meh4 = $("#panel-container").style.min-height;
-
-    //var h = meh.context.document.getElementById('someDiv').style.offsetHeight;
-
+    this.panelWidth = this.getPanelWidth();
+    this.panelHeight = this.getPanelHeight();
 
     var data = d3.range(1000).map(d3.randomBates(10));
-
     var formatCount = d3.format(",.0f");
 
     var margin = {top: 10, right: 0, bottom: 30, left: 0};
-    //var width = this.panelWidth - margin.left - margin.right;
     var width = this.panelWidth;
-    var height = this.panelHeight; // - margin.top - margin.bottom;
+    var height = this.panelHeight;
     console.log("panelWidth = " + width + " height = " + height);
-    // this is a grafana minimum for a panel
-    if (height < 250) {
-      this.panelHeight = 250;
-      height = 250;
-    }
-    //width = 1000;
-    //height=400;
-    //height = 325;
-    console.log("init panel height " + height);
     var x = d3.scaleLinear()
         .rangeRound([0, width]);
 
     var bins = d3.histogram()
         .domain(x.domain())
-        .thresholds(x.ticks(20))
+        .thresholds(x.ticks(10))
         (data);
 
     var y = d3.scaleLinear()
         .domain([0, d3.max(bins, function(d) { return d.length; })])
         .range([height, 0]);
 
-//        .attr("width", width + margin.left + margin.right)
-//        .attr("height", height + margin.top + margin.bottom)
-
-
-//.attr("width", width + margin.left + margin.right)
-//.attr("height", height + margin.top + margin.bottom)
-//.attr("viewBox", "0 0 " + width + " " + height)
-
-/*
-var svg = d3.select(this.panelContainer[0])
-    .append("div")
-    .classed("svg-container", true) //container class to make it responsive
-    .append("svg")
-    .attr("width", 800)
-    .attr("height", 500)
-    .attr("preserveAspectRatio", "xMinYMin meet")
-    .attr("viewBox", "0 0 " + width + " 500")
-    .attr("id", "mystuff")
-    .classed("svg-content-responsive", true)
-    .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-*/
-//.attr("preserveAspectRatio", "xMidYMid meet")
-//.attr("viewBox", "0 0 600 450")
+    // clear out the old div
+    var svgElement = $("#mystuff");
+    svgElement.replaceWith("");
     var svg = d3.select(this.panelContainer[0])
         .append("div")
         .classed("svg-container", true) //container class to make it responsive
         .append("svg")
         .attr("width", width + "px")
-        .attr("height", (height + 50) + "px")
+        .attr("height", (height + 24) + "px")
         .attr("id", "mystuff")
         .classed("svg-content-responsive", true)
         .append("g")
@@ -182,13 +110,13 @@ var svg = d3.select(this.panelContainer[0])
 
     var bar = svg.selectAll(".bar")
         .data(bins)
-      .enter().append("g")
+        .enter().append("g")
         .attr("class", "bar")
         .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; });
 
     bar.append("rect")
         .attr("x", 1)
-        .attr("width", x(bins[0].x1) - x(bins[0].x0) - 1)
+        .attr("width", x(bins[0].x1) - x(bins[0].x0) - 3)
         .attr("height", function(d) { return height - y(d.length); });
 
     bar.append("text")
@@ -203,15 +131,15 @@ var svg = d3.select(this.panelContainer[0])
         .attr("transform", "translate(0," + height + ")")
         .call(d3.axisBottom(x));
 
-
     this.svg = svg;
-  
-    console.log("leaving initializeD3");
+    console.log("leaving renderD3");
   }
 
   link(scope, elem, attrs, ctrl) {
     console.log("d3graph inside link");
-    d3graphRenderer(scope, elem, attrs, ctrl);
+    ctrl.setContainer(elem.find('.panel-content'));
+    // force a render
+    this.onRender();
   }
 
 }
